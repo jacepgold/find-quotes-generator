c0b746e4b972951b78946c9c9dddf603881a5c42
Jace Gold <jacegold@Jaces-MacBook-Pro.local>
2018-11-07 17:20:57 -0700

    FIND Quote Generator JS Lecture

---
+++ app.js
@@ -0,0 +1,28 @@
+var quotes = [
+  "“The Way Get Started Is To Quit Talking And Begin Doing.” -Walt Disney",
+  "“The Pessimist Sees Difficulty In Every Opportunity. The Optimist Sees The Opportunity In Every Difficulty.” -Winston Churchill",
+  "“Don’t Let Yesterday Take Up Too Much Of Today.” -Will Rogers",
+  "“You Learn More From Failure Than From Success. Don’t Let It Stop You. Failure Builds Character.” - Unknown",
+  "“It’s Not Whether You Get Knocked Down, It’s Whether You Get Up.” - Vince Lombardi",
+  "“If You Are Working On Something That You Really Care About, You Don’t Have To Be Pushed. The Vision Pulls You.” - Steve Jobs",
+  "“People Who Are Crazy Enough To Think They Can Change The World, Are The Ones Who Do.” - Rob Siltanen",
+  "“Failure Will Never Overtake Me If My Determination To Succeed Is Strong Enough.” - Og Mandino",
+  "“Entrepreneurs Are Great At Dealing With Uncertainty And Also Very Good At Minimizing Risk. That’s The Classic Entrepreneur.” - Mohnish Pabrai",
+  "“We May Encounter Many Defeats But We Must Not Be Defeated.” - Maya Angelou",
+  "“Knowing Is Not Enough, We Must Apply. Wishing Is Not Enough, We Must Do.” - Johann Wolfgang Von Goethe",
+  "“Imagine Your Life Is Perfect In Every Respect, What Would It Look Like?” - Brian Tracy",
+  "“We Generate Fears While We Sit. We Overcome Them By Action.” - Dr. Henry Link",
+  "“Whether You Think You Can Or Think You Can’t, You’re Right.” - Henry Ford",
+  "“Security Is Mostly A Superstition. Life Is Either A Daring Adventure Or Nothing.” - Helen Keller",
+  "“The Man Who Has Confidence In Himself Gains The Confidence Of Others.” - Hasidic Proverb",
+  "“The Only Limit To Our Realization Of Tomorrow Will Be Our Doubts Of Today.” - Franklin D. Roosevelt",
+  "“Creativity Is Intelligence Having Fun.” - Albert Einstein",
+  "“Dave Jungst Jumped Ship!” - Jace P. Gold",
+  "“What You Lack In Talent Can Be Made Up With Desire, Hustle And Giving 110% All The Time.” - Don Zimmer",
+]
+
+function newQuote() {
+  // alert('hello')
+  var randNum = Math.floor(Math.random() * (quotes.length))
+  document.getElementById('quoteDisplay').innerHTML = quotes[randNum];
+}
